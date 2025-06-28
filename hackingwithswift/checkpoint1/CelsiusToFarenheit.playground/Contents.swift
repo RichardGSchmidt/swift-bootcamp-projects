a4231cc0ca9c39ee341578867c5a80c40116df37
Richard Schmidt <richard.g.schmidt.jr@gmail.com>
2025-06-27 19:07:47 -0500

    Completed checkpoint1.

--- hackingwithswift/checkpoint1/CelsiusToFarenheit.playground/Contents.swift
+++ hackingwithswift/checkpoint1/CelsiusToFarenheit.playground/Contents.swift
@@ -1,3 +1,6 @@
 import UIKit
 
-var greeting = "Hello, playground"
+let temperature = 25.5
+var fahrenheit = 0.0
+fahrenheit = temperature * 9.0 / 5.0 + 32.0
+print("The temperature is \(fahrenheit)F and \(temperature)C")
