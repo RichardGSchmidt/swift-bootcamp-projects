6bcda7831fdfea583d205ba497b0abd19451efe1
Richard Schmidt <richard.g.schmidt.jr@gmail.com>
2025-07-03 12:34:45 -0500

    Completed Checkpoint 2

--- hackingwithswift/checkpoints/Checkpoint2.playground/Contents.swift
+++ hackingwithswift/checkpoints/Checkpoint2.playground/Contents.swift
@@ -1,3 +1,15 @@
 import UIKit
 
-var greeting = "Hello, playground"
+var bands: [String] = ["AC/DC", "Queen", "Led Zeppelin", "Pink Floyd", "The Rolling Stones","Tool","A Perfect Circle", "The Cure", "The Smiths", "Radiohead", "Tool", "Tool"]
+
+
+
+print(bands.count)
+
+var setOfBands:Set<String> = Set(bands)
+
+print(setOfBands.count)
+
+print("List of bands: \(bands) \n Count: \(bands.count) ")
+
+print("Set of bands: \(setOfBands) \n Unique Count: \(setOfBands.count) ")
