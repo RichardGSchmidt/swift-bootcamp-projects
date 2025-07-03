ae8bf88328d7cb9d27a549848180404cd947188d
Richard Schmidt <richard.g.schmidt.jr@gmail.com>
2025-07-03 12:39:39 -0500

    Completed Checkpoint 3, Fizzbuzz

--- hackingwithswift/checkpoints/FizzBuzz.playground/Contents.swift
+++ hackingwithswift/checkpoints/FizzBuzz.playground/Contents.swift
@@ -1,3 +1,13 @@
 import UIKit
 
-
+for i in 1...100 {
+    if i % 3 == 0 && i % 5 == 0 {
+        print("\(i): FizzBuzz")
+    } else if i % 3 == 0 {
+        print("\(i): Fizz")
+    } else if i % 5 == 0 {
+        print("\(i): Buzz")
+    } else {
+        print("\(i)")
+    }
+}
